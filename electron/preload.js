fa6057c822e48104597a830c94a5068bd16f4912
bkk101 <kbk10106@gmail.com>
2026-03-24 01:49:10 +0900

    Updates

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  saveJsonFile: (payload) => ipcRenderer.invoke('file:save-json', payload),
-  openJsonFile: () => ipcRenderer.invoke('file:open-json'),
-});
--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders quad-to-do header', () => {
-  render(<App />);
-  const heading = screen.getByText(/urgent x important memo board/i);
-  expect(heading).toBeInTheDocument();
-});
